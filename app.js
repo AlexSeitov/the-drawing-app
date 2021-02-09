2c2125b8f48f2a983510846684463024d58fb0b2
AlexSeitov <alexander.seitov@yandex.ru>
2021-02-09 20:21:05 +0300

    completed the drawing app

---
+++ app.js
@@ -0,0 +1,93 @@
+const canvas = document.getElementById('drawing-canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const strokeThickness = document.getElementById('size');
+const colorBtn = document.getElementById('color');
+const clearBtn = document.getElementById('clear');
+
+const ctx = canvas.getContext('2d');
+
+let size = 10;
+let isPressed = false;
+let color = 'black';
+let x = undefined;
+let y = undefined;
+
+canvas.addEventListener('mousedown', function (e) {
+	isPressed = true;
+	x = e.offsetX;
+	y = e.offsetY;
+});
+
+canvas.addEventListener('mouseup', function (e) {
+	isPressed = false;
+	x = undefined;
+	y = undefined;
+});
+
+canvas.addEventListener('mousemove', function (e) {
+	if (isPressed) {
+		const x2 = e.offsetX;
+		const y2 = e.offsetY;
+
+		drawCircle(x2, y2);
+		drawLine(x, y, x2, y2);
+		x = x2;
+		y = y2;
+	}
+});
+
+// Drawing Lines
+function drawLine(x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = size * 2;
+	ctx.stroke();
+}
+
+// Drawing Circles
+function drawCircle(x, y) {
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, Math.PI * 2);
+	ctx.fillStyle = color;
+	ctx.fill();
+}
+
+// Increase Btn
+increaseBtn.addEventListener('click', function () {
+	size += 1;
+
+	if (size > 50) {
+		size = 50;
+	}
+
+	updateSize();
+});
+
+// Decrease Btn
+decreaseBtn.addEventListener('click', function () {
+	size -= 1;
+
+	if (size < 1) {
+		size = 1;
+	}
+
+	updateSize();
+});
+
+// Color Btn
+colorBtn.addEventListener('change', function (e) {
+	color = e.target.value;
+});
+
+// Clear Btn
+clearBtn.addEventListener('click', function (e) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+// Updating the Stroke width dynamically
+function updateSize() {
+	strokeThickness.innerText = size;
+}
